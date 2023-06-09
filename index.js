d6bfcd293963aed777968d272307e287ef7a1a2b
alwaysalty <cenkerdonmez@gmail.com>
2023-06-09 18:18:06 +0300

    done

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  function isPalindrome(word):
+    for i from 0 to word.length / 2:
+        j = word.length - 1 - i
+        if word[i] is not equal to word[j]:
+            return false
+    
+    return true
+
 */
 
 /*
@@ -13,13 +26,34 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+ console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+console.log("Expecting: true");
+console.log("=>", isPalindrome("mom"));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("abba"));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("a"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("hi"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
+
 }
 
 module.exports = isPalindrome;
